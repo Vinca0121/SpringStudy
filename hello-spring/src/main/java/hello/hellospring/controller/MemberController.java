a150bb2619ba63c6655ef89085a035b00772ddd9
Vinca0121 <qqw7820@gmail.com>
2023-03-09 17:48:20 +0900

    Add CreateMemberForm

--- hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,11 +1,19 @@
 package hello.hellospring.controller;
 
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.logging.Logger;
+
 
 @Controller
+@Slf4j
 public class MemberController {
 
     private final MemberService memberservice;
@@ -16,5 +24,21 @@ public class MemberController {
         this.memberservice = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    // MemberFrom 데이터 클래스라고 생각하면 된다.
+    public String create(MemberForm form){
+        Member member = new Member();
+        System.out.println(form.getName() + "<<form에 저장된 getName 값");
+        log.info(form.getName(), "<<form에 저장된 getName 값");
+        member.setName(form.getName());
+        memberservice.join(member);
+
+        return "redirect:/";
+    }
 
 }
---
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,16 @@
+package hello.hellospring.controller;
+
+public class MemberForm {
+    private String zname12345;
+
+
+    public String getName() {
+        return zname12345;
+    }
+
+    public void setNameZZZZ(String name12345) {
+        this.zname12345 = name12345;
+    }
+}
+
+
