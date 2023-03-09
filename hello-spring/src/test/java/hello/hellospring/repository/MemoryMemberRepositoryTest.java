abb7e0414f2ce1bb93c8b61ea002cededbfccb2d
Vinca0121 <qqw7820@gmail.com>
2023-03-09 10:40:51 +0900

    Service create!

--- hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -3,13 +3,23 @@ package hello.hellospring.repository;
 import hello.hellospring.domain.Member;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 class MemoryMemberRepositoryTest {
 
     MemoryMemberRepository repository= new MemoryMemberRepository();
+
+    //메소드가 끝날 때마다 호출되는 동작
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
+
 
     @Test
     public void save(){
@@ -33,6 +43,21 @@ class MemoryMemberRepositoryTest {
 
         Member member_got = repository.findByName("rockwell").get();
         assertThat(member1).isEqualTo(member_got);
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+        assertThat(result.size()).isEqualTo(2);
+
     }
 
 }
